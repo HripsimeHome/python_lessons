00388c9e9636cc19a369dff75e59c0030d6e19c6
Hripsime <m_hripsime@mail.ru>
2019-09-29 03:24:08 +0500

    Added Homework1, Homework2

---
+++ hw1.py
@@ -0,0 +1,21 @@
+import math
+
+#4. Greet user with his name inputed from console
+print("Please, enter your Name")
+username = input()
+print("Hello, " + username)
+
+#6. Write a square root calculator that performs for some inputed value
+x = 25
+print(math.sqrt(x))
+
+# 9. Write some comments to your code
+'''
+This is a multiline 
+comment
+'''
+
+"""
+This is another  
+comments
+"""
---
+++ module_task.py
@@ -0,0 +1,8 @@
+# *Create 2 modules: 1. stores some attributes about a room (color, number, etc.); 2. makes some operations on attributes of first module
+number1 = 10
+number2 = 20
+print(number1 + number2)
+
+g = "green"
+b = "blue"
+print("Room's colors are " + g + " and", b)
