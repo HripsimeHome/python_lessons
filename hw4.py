f52888abaa1e7de31f144431f05110cf32717bf1
Hripsime <m_hripsime@mail.ru>
2019-10-06 00:02:56 +0500

    Added homework4

---
+++ hw4.py
@@ -0,0 +1,50 @@
+import math
+#Define a collection of weekdays, print following: first 5 week days, total days number, last 2 days, odd days
+
+weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+print(weekdays[:5])
+print(len(weekdays))
+print(weekdays[-2:])
+
+
+#Define a collection of colors, add a color from console, print total colors number, print colors on even positions
+
+colors = ["Red", "Green", "Blue", "Gray", "Yellow", "White"]
+color = input('Please, enter color name: ')
+colors.append(color)
+print(colors)
+print(len(colors))
+print(colors[1::2])
+
+
+#Define collection of students (names) from group 1 and collection of students from group 2, print the collection of students from both groups
+
+group1 = ["Ann", "Bob", "Sam"]
+group2 = ["Jhon", "Tom", "Kevin"]
+print(group1 + group2)
+
+
+# Define a collection of programming languages (names), add new language name from console if it’s not C#
+languages = ["Python", "Java"]
+language = input("Please, enter the language:")
+languages.append(language)
+print(languages)
+
+
+#Define an empty collection, input 3 numbers and add them to collection, calculate factorial for the last one
+
+numbers = []
+n = int(input('Enter the number: '))
+numbers.append(n)
+print(math.factorial(n))
+
+
+#Define a collection of words, input 2 new words and add them to list, find some inputed word in list and remove a word with some inputed index
+
+words = ["Large", "Big", "Small", "Tall"]
+word = input("Please, enter a word:")
+words.append(word)
+print(words)
+print(words[3])
+words.pop(2)
+print(words)
