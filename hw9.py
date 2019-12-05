20d1b292eb3ca023d2f9c47703532495f6b1fa96
Hripsime <m_hripsime@mail.ru>
2019-12-06 00:59:07 +0400

    Added Ex1 to Lesson 9

--- hw9.py
+++ hw9.py
@@ -2,47 +2,109 @@
 # how many numbers are greater than 20 for second and how many numbers are greater than 30 for third collection
 # (implement version without functions and version with functions)
 
-# Version without function
-
-# collection = []
-# count = 0
-# for i in range(3):
-#     print("Please, input number to the first collection: ")
-#     first_collection = int(input())
-#     # collection.append(first_collection)
-#     if i > 10:
-#         count +=1
-# print(count)
-
-
+# Version without function:
 
 # first_collection = []
-# print("Please, input 3 numbers: ")
-# count = 0
-# for i in range(3):
-#     num = int(input())
-#     first_collection.append(num)
+# second_collection = []
+# third_collection = []
 #
-# check_collection = [num for num in first_collection if num >10]
-# count += 1
-# print(count)
-
-
+# count1 = 0
+# count2 = 0
+# count3 = 0
+#
+# print ("Please, input 3 numbers for the first collection: ")
+# for i in range(3):
+#     number1 = int(input())
+#     first_collection.append(number1)
+#     if number1 > 10:
+#         count1 +=1
+#
+# print ("Please, input 3 numbers for the second collection: ")
+# for i in range(3):
+#     number2 = int(input())
+#     second_collection.append(number2)
+#     if number2 > 20:
+#         count2 += 1
+#
+# print ("Please, input 3 numbers for the third collection: ")
+# for i in range(3):
+#     number3 = int(input())
+#     third_collection.append(number3)
+#     if number3 > 30:
+#         count3 += 1
+# print("Greater than 10: ", count1)
+# print("Greater than 20: ", count2)
+# print("Greater than 30: ", count3)
 
 
 # Version with functions
 
-numbers = ()
-
-print("Please, input 3 numbers: ")
-def number_coounter(number):
-    count = 0
-    for i in number():
-        num = int(input())
-        if num > 10:
-            count +=1
-            print(count)
+# numbers = ()
+#
+# print("Please, input 3 numbers: ")
+# def number_coounter(number):
+#     count = 0
+#     for i in number():
+#         num = int(input())
+#         if num > 10:
+#             count +=1
+#             print(count)
 # number_coounter(number)
+
+
+
+# print ("Please, enter the number")
+# def greater(first_collection):
+#     count = 0
+#     for i in range(2):
+#         num = int(input())
+#         if i > 10:
+#             count += 1
+#     return count
+#     print(count)
+
+
+
+#rabotayet tak
+# first_collection = (2, 3, 4, 20, 30, 40)
+# def greater(first_collection):
+#     count = 0
+#     for i in first_collection:
+#         if i > 10:
+#             count += 1
+#     return count
+# print(greater(first_collection))
+
+
+
+
+
+#
+# print ("Please, input 3 numbers for the third collection: ")
+# def greater(first_collection):
+#     count = 0
+#     for i in first_collection:
+#         num = int(input())
+#         if num > 10:
+#             count += 1
+#     return count
+# print(greater(first_collection))
+
+
+
+
+
+# print ("Please, input 3 numbers for the third collection: ")
+#
+# def even_numbers(numbers):
+#     sum = 0
+#     for number in numbers:
+#         num = int(input())
+#         if number > 10:
+#             sum += 1
+#             print(sum)
+#
+# even_numbers(sum)
 
 
 
