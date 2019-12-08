cfa4204d6555ab56532fae7e087d0732298eefae
Hripsime <m_hripsime@mail.ru>
2019-12-09 00:46:43 +0400

    Added hw9 Ex1, Ex2, Ex3 half

--- hw9.py
+++ hw9.py
@@ -39,88 +39,110 @@
 
 # Version with functions
 
-# numbers = ()
+# first_collection = []
+# second_collection = []
+# third_collection = []
 #
-# print("Please, input 3 numbers: ")
-# def number_coounter(number):
+# def greater_10(numbers):
 #     count = 0
-#     for i in number():
-#         num = int(input())
-#         if num > 10:
-#             count +=1
-#             print(count)
-# number_coounter(number)
-
-
-
-# print ("Please, enter the number")
-# def greater(first_collection):
-#     count = 0
-#     for i in range(2):
-#         num = int(input())
-#         if i > 10:
-#             count += 1
-#     return count
-#     print(count)
-
-
-
-#rabotayet tak
-# first_collection = (2, 3, 4, 20, 30, 40)
-# def greater(first_collection):
-#     count = 0
-#     for i in first_collection:
-#         if i > 10:
-#             count += 1
-#     return count
-# print(greater(first_collection))
-
-
-
-
-
-#
-# print ("Please, input 3 numbers for the third collection: ")
-# def greater(first_collection):
-#     count = 0
-#     for i in first_collection:
-#         num = int(input())
-#         if num > 10:
-#             count += 1
-#     return count
-# print(greater(first_collection))
-
-
-
-
-
-# print ("Please, input 3 numbers for the third collection: ")
-#
-# def even_numbers(numbers):
-#     sum = 0
+#     print("Please, enter 3 numbers to the first collection: ")
+#     for i in range(3):
+#         input_number = int(input())
+#         numbers.append(input_number)
 #     for number in numbers:
-#         num = int(input())
 #         if number > 10:
-#             sum += 1
-#             print(sum)
+#             count += 1
+#     return count
 #
-# even_numbers(sum)
-
+# def greater_20(numbers):
+#     count = 0
+#     print("Please, enter 3 numbers to the second collection: ")
+#     for i in range(3):
+#         input_number = int(input())
+#         numbers.append(input_number)
+#     for number in numbers:
+#         if number > 20:
+#             count += 1
+#     return count
+#
+# def greater_30(numbers):
+#     count = 0
+#     print("Please, enter 3 numbers to the third collection: ")
+#     for i in range(3):
+#         input_number = int(input())
+#         numbers.append(input_number)
+#     for number in numbers:
+#         if number > 30:
+#             count += 1
+#     return count
+# print("Grater than 10:", greater_10(first_collection))
+# print("Grater than 20:", greater_20(second_collection))
+# print("Grater than 30:", greater_30(third_collection))
 
 
 # 2. Input a collection of numbers, calculate how many numbers are positive and dividable by 6 (create a function),
 # generate a new collection of odd numbers (create a function), print results
+#
+# numbers_list = []
+# def check_numbers(numbers):
+#     count = 0
+#     print("Please, enter 3 numbers to check positive numbers: ")
+#     for i in range(3):
+#         input_number = int(input())
+#         numbers.append(input_number)
+#     for number in numbers:
+#         if number > 0 and number % 6 == 0:
+#             count +=1
+#     return count
+# print("Positive and dividable by 6: ", check_numbers(numbers_list))
 
 
-# digits = ()
-# #input
-# def positiv_devide(digs):
-#     sum = 0
-#     # input#input
-#     for i in digs:
-#         if i < 0 and i %6 == 0:
-#             sum += 1
-#     return sum
-# print(positiv_devide(i))
+# generate a new collection of odd numbers
+# main_list = [1, 3, 5, 8, 56, 79]
+# def odd_numbers(numbers):
+#     new_list = []
+#     for number in numbers:
+#         if number % 2 == 1:
+#             new_list.append(number)
+#     return new_list
+# print(odd_numbers(main_list))
 
 
+# 3. Define a collection of company employees (full name(‘Jack Smith’), position),
+# calculate how many people with name ‘John’ work in company (create a function),
+# calculate how many people have ‘Engineer’ position (create a function), print results
+
+
+# calculate how many people have ‘Engineer’ position
+employee = {
+    "Jack Smith"  : "Engineer",
+    "John Taylor" : "Developer",
+    "David Scott" : "Engineer",
+    "John Parker" : "Engineer",
+    "Denis Smith" : "Manager",
+}
+def position_calculate(name, position):
+    count = 0
+    for i in name.values():
+       if i == position:
+         count += 1
+    return count
+print("There are", position_calculate(employee, "Engineer"), "Engineers.")
+
+
+# calculate how many people with name ‘John’ work in company
+
+employee = {
+    "Jack Smith"  : "Engineer",
+    "John Taylor" : "Developer",
+    "David Scott" : "Engineer",
+    "John Parker" : "Engineer",
+    "Denis Smith" : "Manager",
+}
+def name_calculate(position, name="John"):
+    count = 0
+    for i in position.keys():
+        if i == name:
+            count += 1
+    return count
+print("There are", name_calculate(employee), "with this name.")
